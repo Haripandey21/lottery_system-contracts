29044ab39b78dce9d8aad46f16f2bd16d3126d0e
Hari Pandey <69706767+Harry-code21@users.noreply.github.com>
2022-09-07 13:25:29 +0545

    openlottery test

--- contracts/lotteryMethods.sol
+++ contracts/lotteryMethods.sol
@@ -1,63 +1,65 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9; 
+pragma solidity ^0.8.9;
 
 import "./lotteryData.sol";
 
 contract lotteryMethods is lotteryData {
+    // opening lottery by Manager .......
+    function openLottery(
+        uint256 _openTime,
+        uint256 _closeTime,
+        uint256 _lotteryPrice
+    ) public onlyManager {
+        openTime = _openTime;
+        closeTime = _closeTime;
+        lotteryPrice = _lotteryPrice;
+        isLotteryOpen = true;
+    }
 
+    // generate random number ......
+    function randomNumbergenerate() public view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.difficulty,
+                        block.timestamp,
+                        participants.length
+                    )
+                )
+            );
+    }
 
-// opening lottery by Manager .......
-function openLottery (uint _openTime, uint _closeTime,uint _lotteryPrice) onlyManager public {  
-    openTime=_openTime;
-    closeTime=_closeTime;
-    lotteryPrice=_lotteryPrice;
-    isLotteryOpen=true;
+    // Contract collecting the transfered amount......
+    receive() external payable {}
 
-}
+    function applyLottery(uint256 appliedTime) public payable {
+        require(appliedTime > openTime, "it's not started yet..");
+        require(appliedTime < closeTime, "lottery ended");
+        //  require(msg.sender!=manager,"manager cannot apply...");
+        require(msg.value == lotteryPrice);
+        participants.push(payable(msg.sender));
+        //  payable (address(this)).transfer(msg.value);
+    }
 
-// generate random number ......
-function randomNumbergenerate() public view returns(uint)
-{
-    return uint(keccak256(abi.encodePacked(block.difficulty,block.timestamp,participants.length)));
-}
+    // fecthing balance of contract..........
+    function balanceofContract() public view returns (uint256) {
+        return address(this).balance;
+    }
 
-// Contract collecting the transfered amount......
-receive() external payable {}
+    // announcing winner only  by owner ......
+    function getWinner() public onlyManager {
+        uint256 n = randomNumbergenerate();
+        address winner;
+        uint256 randomindex = n % participants.length;
+        winner = participants[randomindex];
 
+        uint256 c = balanceofContract();
+        //uint256 amt=10^18 * c;
+        uint256 winnerAmount = (67 * c) / 100;
+        uint256 managerAmount = (13 * c) / 100;
+        payable(winner).transfer(winnerAmount);
 
-function applyLottery(uint appliedTime) payable public { 
-   require(appliedTime>openTime,"it's not started yet..");
-   require(appliedTime<closeTime,"lottery ended");
- //  require(msg.sender!=manager,"manager cannot apply...");
-    require(msg.value==lotteryPrice);   
-      participants.push(payable(msg.sender));   
-      //  payable (address(this)).transfer(msg.value);
- 
-}
-
-// fecthing balance of contract..........
-function balanceofContract()  public view returns(uint) {
-    return address(this).balance; 
-
-}
-
-
-// announcing winner only  by owner ......
-function getWinner() public onlyManager {
-
-uint n=randomNumbergenerate();
-address winner;
-uint randomindex = n % participants.length;
-winner=participants[randomindex];
-
-uint256 c= balanceofContract();
-//uint256 amt=10^18 * c; 
-uint256 winnerAmount=(67*c)/100;
- uint managerAmount=(13*c)/100;
-payable(winner).transfer(winnerAmount);
-
-payable(manager).transfer(managerAmount);
-
-}
-
+        payable(manager).transfer(managerAmount);
+    }
 }
--- test/2_openLotteryTest.js
+++ test/2_openLotteryTest.js
@@ -1,19 +1,20 @@
 const { expect } = require("chai");
 const hre = require("hardhat");
 
+let openTime = new Date(2022, 08, 07, 1, 03).getTime();
+let closeTime = new Date(2022, 08, 07, 1, 30).getTime(); 
+
+
 describe("Deployment testing", function () {
   it("deployment should work ", async function () {
 
-    const [owner]= await hre.ethers.getSigners();
-    console.log("owner address",owner.address);
+    [add1, add2,add3,add4,add5] = await hre.ethers.getSigners();
+    contract = await hre.ethers.getContractFactory("lotteryMethods");
     const instancelottery = await hre.ethers.getContractFactory("lotteryMethods");
     const deployedlottery = await instancelottery.deploy();
+   
+    await deployedlottery.connect(add1).openLottery(openTime,closeTime,1200);
     console.log("deployed address : ",deployedlottery.address);
-
-
-    a=await deployedlottery.openLottery(1,11,12);
-    // b=await deployedlottery.participate();
-    // console.log("data : ",b);
 
 
   });
