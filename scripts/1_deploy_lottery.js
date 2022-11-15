db8439e496d011e04b6335c1c15c8a7bc2b028ee
haripandey21 <iamgoing1214@gmail.com>
2022-11-16 07:35:06 +0800

    rename test's folders

--- scripts/1_deploy_lottery.js
+++ scripts/1_deploy_lottery.js
@@ -3,16 +3,8 @@ const { ethers } = require("hardhat");
 async function main() {
   const LotteryContracts = await ethers.getContractFactory("Lottery");
   const deployedLotteryContracts  = await LotteryContracts.deploy();
-  await deployedLotteryContracts.deployed();
-  
-  // .env file
-  // dotenv js package
-  // remove console.log
-  // add address to that file
-  // test case - process.env.LOTTERY_CONTRACT
-
- //deployedContractAddress= process.env.DEPLOYED_CONTRACT_ADDRESS; 
-  console.log(
+  await deployedLotteryContracts.deployed(); 
+    console.log(
     "deployed Contract Address in goerli :",deployedLotteryContracts.address );
 }
 main()
